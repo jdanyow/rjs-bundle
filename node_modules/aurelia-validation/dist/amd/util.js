d9d4ef36d5dba71d77d840d05d2d3bfa5e606185
Jeremy Danyow <jdanyow@gmail.com>
2017-10-04 19:18:19 -0700

    Update 2017-10-05T02:18:19.255Z

---
+++ node_modules/aurelia-validation/dist/amd/util.js
@@ -0,0 +1,8 @@
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    function isString(value) {
+        return Object.prototype.toString.call(value) === '[object String]';
+    }
+    exports.isString = isString;
+});
